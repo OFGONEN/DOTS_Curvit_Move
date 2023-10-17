88391cf6893255491a6297ffa32b4a0c486f305a
ofgonen-adastec <ofgonen@gmail.com>
2023-10-17 10:36:14 +0300

    chore: OSMPrefabAuthoring implement dependency assets

--- Assets/Scripts/AuthoringAndBaking/OSMPrefabAuthoring.cs
+++ Assets/Scripts/AuthoringAndBaking/OSMPrefabAuthoring.cs
@@ -9,9 +9,17 @@ public class OSMPrefabAuthoring : MonoBehaviour
     {
         public override void Bake(OSMPrefabAuthoring authoring)
         {
+            //Dependencies
+            DependsOn(authoring.OSMNodePrefab);
+            
+            
+            if(authoring.OSMNodePrefab == null)
+                return;
+            
             //Prefab Entities
             var NodePrefabEntity = GetEntity(authoring.OSMNodePrefab, TransformUsageFlags.Dynamic);
             
+            //Instantiated Entities
             var osmPrefabPropertyEntity = GetEntity(TransformUsageFlags.None);
             
             AddComponent(osmPrefabPropertyEntity, new OSMPrefabProperties
