9ca92f78d9980fec7390048ddfc59bb719486c1d
ofgonen-adastec <ofgonen@gmail.com>
2023-10-18 16:55:08 +0300

    refactor: Load MAT_Lanelet from Resources folder

--- Assets/Scripts/Mono/ResourceLoader.cs
+++ Assets/Scripts/Mono/ResourceLoader.cs
@@ -7,10 +7,11 @@ public class ResourceLoader : MonoBehaviour
 {
     private void Awake()
     {
-        Resources.Load("Prefabs/Line");
-        Resources.Load("Materials/Mat_Line_Arrow_Bidirectional_Dashed");
-        Resources.Load("Materials/Mat_Line_Arrow_Bidirectional_Solid");
-        Resources.Load("Materials/Mat_Line_Arrow_Common_Dashed");
-        Resources.Load("Materials/Mat_Line_Arrow_Common_Solid");
+        Resources.LoadAsync("Prefabs/Line");
+        Resources.LoadAsync("Materials/MAT_Lanelet");
+        Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Dashed");
+        Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Solid");
+        Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Dashed");
+        Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Solid");
     }
 }
