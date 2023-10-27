b9d02a80d5d3313f12e39b23138e344812af3eae
ofgonen-adastec <ofgonen@gmail.com>
2023-10-27 17:48:48 +0300

    feat: implement MeshExtension SetMeshVertices method

--- Assets/Scripts/ExtensionScripts/MeshExtensions.cs
+++ Assets/Scripts/ExtensionScripts/MeshExtensions.cs
@@ -2,7 +2,7 @@ using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
-public static class MeshExtensions 
+public static class MeshExtensions
 {
     public static Mesh BuildMeshForLanelet(NativeList<float3> leftVertices, NativeList<float3> rightVertices)
     {
@@ -49,6 +49,51 @@ public static class MeshExtensions
         
         int[] triangles = CalculateTrianglesForLanelet(leftVertices.Length + rightVertices.Length, isEqual, vertices);
         return CreateMesh(vertices, triangles);
+    }
+
+    public static void SetMeshVertices(NativeList<float3> leftVertices, NativeList<float3> rightVertices, Mesh mesh)
+    {
+        bool isEqual = leftVertices.Length == rightVertices.Length;
+
+        // Combine in one array
+        // Line left is assigned to even numbers
+        // Line right is assigned to odd numbers
+        NativeArray<float3> vertices;
+        int smallCounter = -1;
+        if (isEqual)
+        {
+            vertices = new NativeArray<float3>(leftVertices.Length * 2, Allocator.Temp);
+            for (var i = 0; i < leftVertices.Length; i++)
+                vertices[i * 2] = leftVertices[i];
+            
+            for (var i = 0; i < rightVertices.Length; i++)
+                vertices[smallCounter += 2] = rightVertices[i];
+        }
+        else
+        {
+            NativeList<float3> bigger;
+            NativeList<float3> smaller;
+
+            if (leftVertices.Length >= rightVertices.Length)
+            {
+                bigger = leftVertices;
+                smaller = rightVertices;
+            }
+            else
+            {
+                bigger = rightVertices;
+                smaller = leftVertices;
+            }
+            
+            vertices = new NativeArray<float3>(bigger.Length * 2, Allocator.Temp);
+            for (int i = 0; i < bigger.Length; i++)
+                vertices[i * 2] = bigger[i];
+            
+            for (int i = 0; i < smaller.Length; i++)
+                vertices[smallCounter += 2] = smaller[i];
+        }
+        
+        mesh.SetVertices(vertices);
     }
     
     private static Mesh CreateMesh(Vector3[] vertices, int[] triangles)
